66a47e828460e084765b468e217c63372304e234
cruden <98847288+ZewOn31@users.noreply.github.com>
2023-07-03 14:33:54 +0300

    Update and rename cherish_veux.mk to awaken_veux.mk

--- awaken_veux.mk
+++ awaken_veux.mk
@@ -8,21 +8,26 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-TARGET_USES_AOSP_RECOVERY := true
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_QUICK_TAP := true
-WITH_GMS := true
-
 # Inherit from veux device
 $(call inherit-product, device/xiaomi/veux/device.mk)
 
-# Inherit some common CherishOS
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+# Inherit some common ProjectAwaken
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
 
-PRODUCT_NAME := cherish_veux
+# GMS
+TARGET_GAPPS_ARCH := arm64
+USE_GAPPS := true
+
+# Extras
+AWAKEN_BUILD_TYPE := unofficial
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_USES_AOSP_RECOVERY := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+
+PRODUCT_NAME := awaken_veux
 PRODUCT_DEVICE := veux
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
@@ -34,8 +39,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="veux-user 12 SKQ1.211006.001 V13.0.9.0.SKCMIXM release-keys"
 
 BUILD_FINGERPRINT := POCO/veux_p_global/veux:12/SKQ1.211006.001/V13.0.9.0.SKCMIXM:user/release-keys
-
-# Maintainer Stuff
-CHERISH_BUILD_TYPE := UNOFFICIAL
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.cherish.maintainer=m1xedd
